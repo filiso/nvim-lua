cde7aafb97c46933eaefa56f2a0d2e8f389aa70f
Filip Sobczak <fsobczak@WinKS236-51.is.localnet>
2023-01-16 12:05:35 +0100

    added visual removed insert mode dap-ui eval expr. keymap

--- init.lua
+++ init.lua
@@ -623,4 +623,4 @@ vim.keymap.set('v', '<leader>ds', [[<ESC>:lua require('dap-python').debug_select
 require('dapui').setup()
 vim.keymap.set('n', '<leader>du', require('dapui').toggle)
 vim.keymap.set('n', '<leader>de', require('dapui').eval)
-vim.keymap.set('i', '<leader>de', require('dapui').eval)
+vim.keymap.set('v', '<leader>de', require('dapui').eval)
