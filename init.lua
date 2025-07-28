67374272348ba230d9556148edfb349fd0d2b2b3
fs <fs@fs>
2025-07-28 13:45:11 +0200

    added CopilotChat

--- init.lua
+++ init.lua
@@ -202,6 +202,20 @@ require("lazy").setup({
     end
   },
 
+  -- Github Copilot integration
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+
   -- -- Codeium (unofficial with nvim-cmp support)
   -- {
   --   "Exafunction/codeium.nvim",
