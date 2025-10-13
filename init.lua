ace5ef9ef52c531c4a4d38304266a75232429970
fs <fs@fs>
2025-10-13 15:53:37 +0200

    added keybinds for telescope buffer deletion and 'second-level' fuzzy search within the current, locked in, search results

--- init.lua
+++ init.lua
@@ -550,11 +550,17 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = {
+              ['<c-enter>'] = 'to_fuzzy_refine',
+            },
+            n = {
+              ['q'] = require('telescope.actions').close,
+              ['dd'] = require('telescope.actions').delete_buffer,
+            },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
